5e0f4c57746aa0b17e174b4f42ae9d5b9f8c2e8c
anthologen <anthologen@gmail.com>
2021-05-01 01:55:28 -0400

    add vue boilerplate

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld msg="Hello World!" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped>
+</style>
