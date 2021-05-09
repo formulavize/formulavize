c6a7f06568188155ca925b9ffadeb66f65edb9f8
anthologen <anthologen@gmail.com>
2021-05-09 00:52:00 -0400

    initialize TextEditor component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <HelloWorld msg="Hello World!" />
+  <TextEditor />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import TextEditor from './components/TextEditor.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TextEditor,
   }
 })
 </script>
---
+++ src/components/TextEditor.vue
@@ -0,0 +1,19 @@
+<template>
+  <div ref="editorview" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { EditorState } from "@codemirror/state"
+import { EditorView } from "@codemirror/view"
+
+export default defineComponent({
+  name: 'TextEditor',
+  mounted(): void {
+    const view: EditorView = new EditorView({
+      state: EditorState.create(),
+      parent: this.$refs.editorview as Element,
+    })
+  },
+})
+</script>
