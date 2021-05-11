ed8abaced6d4be397c5f26be797f1b995bec976a
anthologen <anthologen@gmail.com>
2021-05-11 00:53:45 -0400

    initialize GraphView component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <HelloWorld msg="Hello World!" />
   <TextEditor />
+  <GraphView />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 import TextEditor from './components/TextEditor.vue'
+import GraphView from './components/GraphView.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld,
     TextEditor,
+    GraphView,
   }
 })
 </script>
---
+++ src/components/GraphView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div ref="canvas" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import cytoscape from 'cytoscape'
+
+export default defineComponent({
+  name: 'GraphView',
+  mounted(): void {
+    var cy = cytoscape({container: this.$refs.canvas as HTMLElement})
+  },
+})
+</script>
