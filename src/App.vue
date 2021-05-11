05aced711482b7cbfa15592808f9cfae067b1a5a
anthologen <anthologen@gmail.com>
2021-05-11 01:12:09 -0400

    add splitpanes to App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <HelloWorld msg="Hello World!" />
-  <TextEditor />
-  <GraphView />
+  <splitpanes class="default-theme">
+    <pane>
+      <TextEditor />
+    </pane>
+    <pane>
+      <GraphView />
+    </pane>
+  </splitpanes>
 </template>
 
 <script lang="ts">
@@ -9,11 +15,15 @@ import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 import TextEditor from './components/TextEditor.vue'
 import GraphView from './components/GraphView.vue'
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld,
+    Splitpanes,
+    Pane,
     TextEditor,
     GraphView,
   }
