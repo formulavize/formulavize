15ca9ca4ebf38bee91c0fb5cf5d943652060fcf7
anthologen <anthologen@gmail.com>
2021-05-16 22:03:50 -0400

    add basic TextEditor settings

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -5,13 +5,37 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { EditorState } from "@codemirror/state"
-import { EditorView } from "@codemirror/view"
+import { EditorView, keymap, drawSelection, highlightActiveLine } from "@codemirror/view"
+import { defaultKeymap } from "@codemirror/commands"
+import { lineNumbers, highlightActiveLineGutter } from "@codemirror/gutter"
+import { history, historyKeymap } from "@codemirror/history"
+import { bracketMatching } from "@codemirror/matchbrackets"
+import { rectangularSelection } from "@codemirror/rectangular-selection"
+import { searchKeymap, highlightSelectionMatches } from "@codemirror/search"
 
 export default defineComponent({
   name: 'TextEditor',
   mounted(): void {
-    const view: EditorView = new EditorView({
-      state: EditorState.create(),
+    let state = EditorState.create({
+      extensions: [
+        lineNumbers(),
+        history(),
+        highlightActiveLine(),
+        highlightActiveLineGutter(),
+        drawSelection(),
+        EditorState.allowMultipleSelections.of(true),
+        bracketMatching(),
+        rectangularSelection(),
+        highlightSelectionMatches(),
+        keymap.of([
+          ...defaultKeymap,
+          ...historyKeymap,
+          ...searchKeymap,
+        ]),
+      ]
+    })
+    let view: EditorView = new EditorView({
+      state: state,
       parent: this.$refs.editorview as Element,
     })
   },
