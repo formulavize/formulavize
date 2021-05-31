1de9f907fba0da6ce5336f09b2d0391ea08f1fac
anthologen <anthologen@gmail.com>
2021-05-31 02:26:01 -0400

    add fizLanguage to TextEditor

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -5,13 +5,21 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { EditorState } from "@codemirror/state"
-import { EditorView, keymap, drawSelection, highlightActiveLine } from "@codemirror/view"
+import { 
+  EditorView, keymap, drawSelection, 
+  highlightActiveLine, highlightSpecialChars
+} from "@codemirror/view"
 import { defaultKeymap } from "@codemirror/commands"
 import { lineNumbers, highlightActiveLineGutter } from "@codemirror/gutter"
 import { history, historyKeymap } from "@codemirror/history"
 import { bracketMatching } from "@codemirror/matchbrackets"
 import { rectangularSelection } from "@codemirror/rectangular-selection"
 import { searchKeymap, highlightSelectionMatches } from "@codemirror/search"
+import { foldGutter, foldKeymap } from "@codemirror/fold"
+import { defaultHighlightStyle } from "@codemirror/highlight"
+import { closeBrackets, closeBracketsKeymap } from "@codemirror/closebrackets"
+import { commentKeymap } from "@codemirror/comment"
+import { fizLanguage } from 'lang-fiz'
 
 export default defineComponent({
   name: 'TextEditor',
@@ -27,11 +35,19 @@ export default defineComponent({
         bracketMatching(),
         rectangularSelection(),
         highlightSelectionMatches(),
+        foldGutter(),
+        closeBrackets(),
+        defaultHighlightStyle.fallback,
+        highlightSpecialChars(),
         keymap.of([
           ...defaultKeymap,
           ...historyKeymap,
           ...searchKeymap,
+          ...foldKeymap,
+          ...closeBracketsKeymap,
+          ...commentKeymap,
         ]),
+        fizLanguage,
       ]
     })
     let view: EditorView = new EditorView({
