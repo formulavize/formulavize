62b5a9c05f4bbad761941d56e4b78b55818a9011
anthologen <anthologen@gmail.com>
2022-06-14 23:46:30 -0400

    fix ts warning for setTimeout return type

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -29,7 +29,7 @@ export default defineComponent({
   emits: ["update-editorstate"],
   data() {
     return {
-      prevTimeoutId: -1
+      prevTimeoutId: setTimeout(() => {})
     }
   },
   mounted() {
