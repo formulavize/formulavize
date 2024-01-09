ea22192f938cdcf21d79296c8046edfd5a8454ab
anthologen <anthologen@gmail.com>
2024-01-09 00:43:18 -0500

    remove @types/splitpanes due to vue dependency conflict

--- src/App.vue
+++ src/App.vue
@@ -43,6 +43,7 @@ import { RecipeTreeNode } from "./common/ast"
 import { Dag } from './common/dag'
 import { makeDag } from './common/dagFactory'
 import { fillTree } from './common/astFactory'
+// @ts-ignore: remove once @types/splitpanes upgrades dependency to vue 3
 import { Splitpanes, Pane } from 'splitpanes'
 import 'splitpanes/dist/splitpanes.css'
 import './tabs-component.css';
