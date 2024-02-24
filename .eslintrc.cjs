e969e167a363a54048fd717460f619995f6dbd29
anthologen <anthologen@gmail.com>
2024-02-24 16:51:01 -0500

    add ESLint rule to ignore unused _variables

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,4 +11,18 @@ module.exports = {
     "@vue/typescript",
   ],
   plugins: ["vue"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
 };
