201b4dc58c6c09bdabd62a3de465c81b7e9e43ee
anthologen <anthologen@gmail.com>
2025-01-01 17:30:53 -0500

    build: update to eslint 9

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig as testConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
+const config = defineConfig({
   plugins: [Vue()],
+});
+
+const testCfg = testConfig({
   test: {
     include: ["tests/**/*.test.ts"],
   },
 });
+
+export default {
+  ...config,
+  ...testCfg,
+};
