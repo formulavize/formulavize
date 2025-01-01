89eda421a0280b32a2ec05759daa59eab2400379
anthologen <anthologen@gmail.com>
2025-01-01 17:30:53 -0500

    build: update to eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import eslint from "@eslint/js";
+import eslintPluginVue from "eslint-plugin-vue";
+import vueTsEslintConfig from "@vue/eslint-config-typescript";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+  {
+    ignores: ["node_modules/", "dist/"],
+  },
+  {
+    files: ["*.ts", "**/*.ts", "*.vue", "**/*.vue"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...eslintPluginVue.configs["flat/recommended"],
+      ...vueTsEslintConfig(),
+    ],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  eslintPluginPrettierRecommended,
+);
