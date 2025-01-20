7aab4e69b8977aaaf036b9418c74b5e66aedf3f9
anthologen <anthologen@gmail.com>
2025-01-20 02:11:17 -0500

    feat(toolbar): add debugMode toggle button

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,31 @@
 import { createApp } from "vue";
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+
 // @ts-expect-error: remove when vue3-tabs-component types are added
 import { Tabs, Tab } from "vue3-tabs-component";
+
 import App from "./App.vue";
 
-createApp(App).component("tabs", Tabs).component("tab", Tab).mount("#app");
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
+createApp(App)
+  .use(vuetify)
+  .component("tabs", Tabs)
+  .component("tab", Tab)
+  .mount("#app");
