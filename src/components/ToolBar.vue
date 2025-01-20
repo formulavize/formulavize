d0c88985b33a1d80b3681230a56962273b90076d
anthologen <anthologen@gmail.com>
2025-01-19 20:34:55 -0500

    feat(toolbar): add app title to toolbar

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="toolbar">
+    <img id="logo" src="/assets/formulavize_logo.svg" alt="Formulavize logo" />
+    <h1>formulavize</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ToolBar",
+});
+</script>
+
+<style>
+.toolbar {
+  display: flex;
+  align-items: center;
+}
+#logo {
+  height: 2em;
+}
+</style>
