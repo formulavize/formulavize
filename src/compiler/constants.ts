84f422d1de032fcd1b887ab91418710be90279b1
anthologen <anthologen@gmail.com>
2025-01-20 23:26:31 -0500

    style(toolbar): organize buttons into button group

--- src/compiler/constants.ts
+++ src/compiler/constants.ts
@@ -4,3 +4,7 @@ export const DESCRIPTION_PREFIX: string = DESCRIPTION_PROPERTY + "-";
 
 // Import constants
 export const FIZ_FILE_EXTENSION: string = ".fiz";
+
+// Image export formats
+export const PNG = "png";
+export const SVG = "svg";
