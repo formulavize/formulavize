5afb382f05e648ece8a3084622e3d4a2c156c1c6
anthologen <anthologen@gmail.com>
2025-07-26 14:37:47 -0400

    build: add src alias to vite config
    add src alias to help resolve imports VSCode Testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig as testConfig } from "vitest/config";
 import { defineConfig } from "vite";
+import path from "path";
 import Vue from "@vitejs/plugin-vue";
 
 const config = defineConfig({
   plugins: [Vue()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+    },
+  },
 });
 
 const testCfg = testConfig({
