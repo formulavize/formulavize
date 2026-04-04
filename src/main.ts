df3d95809919bc08b0bc70370e543c38dfb23453
anthologen <anthologen@gmail.com>
2026-04-04 16:40:00 -0400

    chore(deps): upgrade dependencies

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,9 @@ import App from "./App.vue";
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: "light",
+  },
   icons: {
     defaultSet: "mdi",
     aliases,
